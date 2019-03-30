223f2cd9cd9c2c22b13ed752c2f016e92b5abfc0
Fyodor <fyodor.work@gmail.com>
2019-03-30 09:03:03 +0200

    custom post styles start + db

---
+++ wp-content/themes/borsa/category-typeone.php
@@ -0,0 +1,6 @@
+<?php
+
+get_header();
+
+?>
+<h1>type one</h1>
---
+++ wp-content/themes/borsa/single-typone.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Template Name: Мой шаблон страницы
+Template Post Type: post, page, product
+*/
+
+get_header();
+
+?>
+<h1>single type one</h1>
